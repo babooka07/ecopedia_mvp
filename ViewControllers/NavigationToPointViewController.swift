27269328da3cef8128a815849635b5165de6cd6c
Anatoliy Anatolyev <>
2018-12-25 19:43:21 +0200

    navigation screen refactored

--- ViewControllers/NavigationToPointViewController.swift
+++ ViewControllers/NavigationToPointViewController.swift
@@ -12,9 +12,16 @@ import CoreLocation
 
 class NavigationToPointViewController: UIViewController {
 
+    @IBOutlet weak var noGPSdataView: UIView!
+    
+    @IBOutlet weak var gpsSignalIndicator_1: UIView!
+    @IBOutlet weak var gpsSignalIndicator_2: UIView!
+    @IBOutlet weak var gpsSignalIndicator_3: UIView!
+    @IBOutlet weak var gpsSignalIndicator_4: UIView!
     
     @IBOutlet weak var navigationArrowView: UIImageView!
     
+    @IBOutlet weak var accuracyLabel: UILabel!
     @IBOutlet weak var distanceLabel: UILabel!
     var currentItemModel = ItemModel()
     
@@ -42,16 +49,87 @@ class NavigationToPointViewController: UIViewController {
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.startUpdatingLocation()
         locationManager.startUpdatingHeading()
+        
+        noGPSdataView.alpha = 0
+        
+        UIApplication.shared.isIdleTimerDisabled = true
+        
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedAlways:
+            break
+        case .authorizedWhenInUse:
+            break
+        default:
+            break
+        }
 
         
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        UIApplication.shared.isIdleTimerDisabled = false
+
+    }
+    
     private func updateDistance () {
         distanceLabel.text = "Distance: \(Int(currentLocation.distance(from: destinationLocation))) m"
+        
     }
+    
+    private func updateAccuracy() {
+        
+        accuracyLabel.text = "Accuracy: ± \(Int(currentLocation.horizontalAccuracy)) m"
+       
+        if currentLocation.horizontalAccuracy < 0 || currentLocation.horizontalAccuracy >= 20
+        {
+            gpsSignalIndicator_1.alpha = 1
+            gpsSignalIndicator_2.alpha = 0
+            gpsSignalIndicator_3.alpha = 0
+            gpsSignalIndicator_4.alpha = 0
+            
+            gpsSignalIndicator_1.backgroundColor = UIColor.red
+        }
+        else if currentLocation.horizontalAccuracy >= 15
+        {
+            gpsSignalIndicator_1.alpha = 1
+            gpsSignalIndicator_2.alpha = 1
+            gpsSignalIndicator_3.alpha = 0
+            gpsSignalIndicator_4.alpha = 0
+            
+            gpsSignalIndicator_1.backgroundColor = UIColor.yellow
+            gpsSignalIndicator_2.backgroundColor = UIColor.yellow
+            
+        }
+        else if currentLocation.horizontalAccuracy >= 10
+        {
+            gpsSignalIndicator_1.alpha = 1
+            gpsSignalIndicator_2.alpha = 1
+            gpsSignalIndicator_3.alpha = 1
+            gpsSignalIndicator_4.alpha = 0
+            
+            gpsSignalIndicator_1.backgroundColor = UIColor.yellow
+            gpsSignalIndicator_2.backgroundColor = UIColor.yellow
+            gpsSignalIndicator_3.backgroundColor = UIColor.yellow
+        }
+        else
+        {
+            gpsSignalIndicator_1.alpha = 1
+            gpsSignalIndicator_2.alpha = 1
+            gpsSignalIndicator_3.alpha = 1
+            gpsSignalIndicator_4.alpha = 1
+            
+            gpsSignalIndicator_1.backgroundColor = UIColor.green
+            gpsSignalIndicator_2.backgroundColor = UIColor.green
+            gpsSignalIndicator_3.backgroundColor = UIColor.green
+            gpsSignalIndicator_4.backgroundColor = UIColor.green
+            
+        }
+    }
+    
     
     private func setDirection() {
         let direction = (getBearingBetweenTwoPoints() - currentNorthDegreeseAngle) * .pi/180
-        UIView.animate(withDuration: 0.5) {
+        UIView.animate(withDuration: Consts.serviceViewAnimationsDuration) {
             self.navigationArrowView.transform = CGAffineTransform(rotationAngle: CGFloat(direction))
         }
         
@@ -86,10 +164,16 @@ class NavigationToPointViewController: UIViewController {
 }
 
 extension NavigationToPointViewController: CLLocationManagerDelegate {
+    
+    func locationManagerShouldDisplayHeadingCalibration(_ manager: CLLocationManager) -> Bool {
+        return true
+    }
+    
     func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
         
         currentNorthDegreeseAngle = newHeading.trueHeading
         updateDistance()
+        updateAccuracy()
         //MARK: TODO сделать безопасным
         setDirection()
     }
@@ -97,9 +181,19 @@ extension NavigationToPointViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.last else {return}
         currentLocation = location
-//     print(location.coordinate.latitude, location.coordinate.longitude)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        if let error = error as? CLError, error.code == .denied {
+            switch error.code {
+                
+                
+                
+            default:
+                break
+            }
+        }
     }
     
 }
-
 
